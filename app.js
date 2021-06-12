4fce5abeb809b339e73ad6b9d218b92126d403de
C0d3president <kadimasam@gmail.com>
2021-06-12 18:29:15 +0300

    Create a variable to store results

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ function akanNameCalculator() {
     event.preventDefault();
 
    let birthDate = parseInt(document.getElementById("birthDate").nodeValue);
+   let gender = document.getElementById("gender").nodeValue;
+   let results = document.getElementById("results").innerHTML;
 
-   
+
 }
