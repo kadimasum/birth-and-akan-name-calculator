145ee63aca1e6dcde40a5e4b0212806f1dbe17a2
C0d3president <kadimasam@gmail.com>
2021-06-12 20:35:14 +0300

    Add and store male akan names in the maleAkan variable

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ function akanNameCalculator() {
 
     event.preventDefault();
 
-   let birthDate = parseInt(document.getElementById("birthDate").value);
+    // Collect form input
+
+   let inputDate = document.getElementById("birthDate").value;
    let male = document.getElementById("genderMale").value;
    let female = document.getElementById("genderFemale").value;
     
+    // Get day
 
-   document.getElementById("results").innerHTML = male;
+   let date = new Date(inputDate);
+   let birthDate = date.getDay();
+
+    // Store Akan names in Arrays
+
+    let maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleAkan = [];
+
+   document.getElementById("results").innerHTML = birthDate;
 
 
 }
