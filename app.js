8121b5091b476a86d65130768e350fe6cddaf9cf
C0d3president <kadimasam@gmail.com>
2021-06-12 20:40:43 +0300

    Add female akan names to the femaleAkan variable

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ function akanNameCalculator() {
 
     // Collect form input
 
-   let inputDate = document.getElementById("birthDate").value;
-   let male = document.getElementById("genderMale").value;
-   let female = document.getElementById("genderFemale").value;
-    
+    let inputDate = document.getElementById("birthDate").value;
+    let male = document.getElementById("genderMale").value;
+    let female = document.getElementById("genderFemale").value;
+        
     // Get day
 
-   let date = new Date(inputDate);
-   let birthDate = date.getDay();
+    let date = new Date(inputDate);
+    let birthDate = date.getDay();
 
     // Store Akan names in Arrays
 
     let maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    let femaleAkan = [];
+    let femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
    document.getElementById("results").innerHTML = birthDate;
 
