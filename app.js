42f7dc16691449d93b116ae220b9a0921c527aee
C0d3president <kadimasam@gmail.com>
2021-06-12 22:49:26 +0300

    Add conditions to find female and male akan

--- app.js
+++ app.js
@@ -4,31 +4,41 @@ function akanNameCalculator() {
 
     // Collect form input
 
+
     let inputDate = document.getElementById("birthDate").value;
-    let male = document.getElementById("genderMale").value;
-    let female = document.getElementById("genderFemale").value;
+    let gender = document.getElementsByName("gender");
+   
         
     // Get day
 
+    
     let date = new Date(inputDate);
     let birthDate = date.getDay();
+     
 
     // Store Akan names in Arrays
+
 
     let maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     let femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+    
     // Find male Akan from birthDate
 
-    for (let i = 0; i = maleAkan.length; i++) {
-        if (male = true) {
-            let result = maleAkan[birthDate];
-            return result;
+    
+    if (document.getElementById("genderMale").checked) {
+
+            document.getElementById("results").innerHTML = maleAkan[birthDate];
         }
+        
+        
+    // Find male Akan from birthDate
 
-        document.getElementById("results").innerHTML = result;
-    }
 
+    if (document.getElementById("genderFemale").checked) {
+
+            document.getElementById("results").innerHTML = femaleAkan[birthDate];
+        }
   
 
 
