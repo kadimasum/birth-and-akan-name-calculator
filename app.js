8633675eac091fd90bf9fb12d3aaae55872f22eb
C0d3president <kadimasam@gmail.com>
2021-06-12 18:42:19 +0300

    Create separate selectors for gender male and female

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ function akanNameCalculator() {
     event.preventDefault();
 
    let birthDate = parseInt(document.getElementById("birthDate").value);
-   let gender = document.getElementById("gender").value;
+   let male = document.getElementById("genderMale").value;
+   let female = document.getElementById("genderFemale").value;
     
 
-   document.getElementById("results").innerHTML = gender;
+   document.getElementById("results").innerHTML = male;
 
 
 }
